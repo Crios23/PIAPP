4660c98d139abea92ded88f468fa36eb340842dd
enzorios <rioscris.291197@gmail.com>
2020-04-22 16:13:25 -0500

    preguntas triaje

---
+++ app/src/main/java/com/example/enzo/altoalcovid/PreguntaTriajeActivity.java
@@ -0,0 +1,13 @@
+package com.example.enzo.altoalcovid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreguntaTriajeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregunta_triaje);
+    }
+}
